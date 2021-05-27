d3747bf9d770da46ec7dddbdc84fda9f29d8f19a
josh-umahi <thejoshumahi@gmail.com>
2021-05-27 15:36:28 -0700

    Minor changes

--- lib/basic_calculator.dart
+++ lib/basic_calculator.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'constants.dart';
-import './components/table_of_buttons.dart';
-import './components/entries_and_results_display.dart';
+import './widgets/table_of_buttons.dart';
+import './widgets/entries_and_results_display.dart';
 
 class BasicCalculatorScreen extends StatelessWidget {
   @override
